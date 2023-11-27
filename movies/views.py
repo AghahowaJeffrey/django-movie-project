fc67636ac9702f5837cb40e3c9c3d921d0a07be9
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2023-11-28 00:43:20 +0100

    made crud functionality in templates

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,49 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
+from .models import Movie
 
 def movies(request):
-    return render(request, 'movies/movies.html', {'movies': ["movie1", "movie2"]}) 
+    data = Movie.objects.all
+    return render(request, 'movies/movies.html', {'movies': data}) 
 
 def home(request):
     return HttpResponse('Home page')
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request, 'movies/detail.html', {'movie': data})
+
+def add(request):
+    title = request.POST.get('title')
+    year = request.POST.get('year')
+
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
+    
+    return render(request, 'movies/add.html')
+
+def delete(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except:
+        raise Http404('Movie does not exist')
+    movie.delete()
+    return HttpResponseRedirect('/movies')
+
+def update(request, id):
+    movie = Movie.objects.get(pk=id)
+    
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        year = request.POST.get('year')
+
+        if title and year:
+            # Update the attributes
+            movie.title = title
+            movie.year = year
+            movie.save()
+            return HttpResponseRedirect('/movies')
+    
+    return render(request, 'movies/update.html')
